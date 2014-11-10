5a2c0910f7affd9afbea208df37604a4aa287fe3
Nathen Harvey <nathen.harvey@gmail.com>
2014-11-10 20:29:17 +0000

    play around with some variable content

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,10 +3,24 @@
 # Recipe:: default
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
+#
+
+foo = "bar"
+
 template "/home/chef/hello.txt" do
   action  :create
   mode    "0777"
   group   "chef"
   source  "hello.txt.erb"
   owner   "chef"
+  variables(
+    :foo => foo
+  )
 end
+
+
+
+
+
+
+
