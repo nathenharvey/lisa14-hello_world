26b09405aec8290440c7ee4ac0230f3ce892d1cd
Nathen Harvey <nathen.harvey@gmail.com>
2014-11-10 20:03:23 +0000

    move the content of hello.txt to a template

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
+template "/home/chef/hello.txt" do
+  action  :create
+  mode    "0777"
+  group   "chef"
+  source  "hello.txt.erb"
+  owner   "chef"
+end
